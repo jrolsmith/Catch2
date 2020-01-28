f118a63a5ca11a137d771bd4efb0c0cf075a38b4
Martin Hořeňovský <martin.horenovsky@gmail.com>
2020-01-28 23:01:31 +0100

    Verbose merge script

--- tools/misc/appveyorMergeCoverageScript.py
+++ tools/misc/appveyorMergeCoverageScript.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python2
 
+from __future__ import print_function
 import glob
 import subprocess
 
 if __name__ == '__main__':
+    print('starting merge')
     cov_files = list(glob.glob('tests/cov-report*.bin'))
+    print('Files to merge:')
+    print(cov_files)
     base_cmd = ['OpenCppCoverage', '--quiet', '--export_type=cobertura:cobertura.xml'] + ['--input_coverage={}'.format(f) for f in cov_files]
+    print('Merge CMD: ',base_cmd)
     subprocess.check_call(base_cmd)
